df5df45d7c444b2c341b030c277761f2d950213b
benjamin chu <benchu99@hotmail.com>
2017-11-11 22:41:04 -0800

    start unit testing for kinship

---
+++ test/MendelKinship_test.jl
@@ -0,0 +1,28 @@
+using MendelKinship
+using MendelBase
+using DataFrames 
+
+@testset "basics" begin
+    
+end
+
+@testset "kinship_matrix" begin
+    
+end
+
+@testset "delta7_matrix" begin
+    
+end
+
+@testset "jacquard_coefficients" begin
+    
+end
+
+@testset "cotwin_extension!" begin
+    
+end
+
+@testset "identity_state" begin
+    
+end
+
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,11 @@
+module PkgTest
 using MendelKinship
 using Base.Test
 
 # write your own tests here
-@test 1 == 1
+include("MendelKinship_test.jl")
+
+
+# julia -e 'Pkg.test("MendelKinship",coverage=true)'
+# @show get_summary(process_file("src/MendelKinship.jl"))
+
