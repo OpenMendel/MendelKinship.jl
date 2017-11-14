d0633d01ffc6250daba23fabed238d0990f5aee6
benjamin chu <benchu99@hotmail.com>
2017-11-14 13:13:40 -0800

    tested identity_state

--- test/MendelKinship_test.jl
+++ test/MendelKinship_test.jl
@@ -20,9 +20,11 @@ using DataFrames
     #
     # The following rules define the kinship coefficient matrix:
     # 1. Φii = 0.5 if i is a founder
-    # 2. Φij = 0 if i, j both founders (i.e. founders are not related)
-    # 3. Φij = 0.5 Φjk + 0.5Φjl when i not founder (i>j), k and l are parents of i
-    # 4. Φii = 0.5 + 0.5Φkl when i not founder, and k, l are parents of i
+    # 2. Φii = 0.5 + 0.5Φkl when i not founder, and k, l are parents of i
+    # 3. Φij = 0 if i, j both founders (i.e. founders are not related)
+    # 4. Φij = 0.5 Φjk + 0.5Φjl when i not founder (i>j), k and l are parents of i
+    # 5. Parents precedes children, and either both parent are present or both aren't, but 
+    #    this was tested in MnedelBase. 
     #
     matrix = MendelKinship.kinship_matrix(pedigree, person, 1, false)
 
@@ -64,24 +66,70 @@ using DataFrames
 
     #6th row & 6th column
     @test matrix[6, 6] == 0.5 + 0.5 * matrix[3, 4] 
-
-    #Too lazy to write tests for another pedigree
-    #matrix2 = MendelKinship.kinship_matrix(pedigree, person, 2, false)
-end
-
-@testset "delta7_matrix" begin
-    
-end
-
-@testset "jacquard_coefficients" begin
-    
-end
-
-@testset "cotwin_extension!" begin
-    
 end
 
 @testset "identity_state" begin
+    #
+    # Testing all possible combinations (hence the weird modular arithmetics). 
+    # Basically match up geometrically on page 10:
+    # http://faculty.washington.edu/tathornt/BIOST551/lectures_2012/Lecture7_Coefficients_of_Identity_and_Coancestry.pdf
+    #
+    for i = 1:4
+        a, b, c, d = i, i + 1, i + 2, i + 3
+        b = mod(b - 1, 4) + 1 # subtract 1, mod, then add one at the end. This skips 0.
+        c = mod(c - 1, 4) + 1
+        d = mod(d - 1, 4) + 1
+
+        @test MendelKinship.identity_state([a, a], [a, a]) == 1
+        @test MendelKinship.identity_state([a, a], [b, b]) == 2
+        @test MendelKinship.identity_state([a, a], [a, b]) == 3
+        @test MendelKinship.identity_state([a, a], [b, a]) == 3
+        @test MendelKinship.identity_state([a, a], [b, c]) == 4
+        @test MendelKinship.identity_state([a, b], [a, a]) == 5
+        @test MendelKinship.identity_state([a, b], [b, b]) == 5
+        @test MendelKinship.identity_state([a, b], [c, c]) == 6
+        @test MendelKinship.identity_state([a, b], [a, b]) == 7
+        @test MendelKinship.identity_state([a, b], [b, a]) == 7
+        @test MendelKinship.identity_state([a, b], [a, c]) == 8
+        @test MendelKinship.identity_state([a, b], [c, a]) == 8
+        @test MendelKinship.identity_state([a, b], [b, c]) == 8
+        @test MendelKinship.identity_state([a, b], [c, b]) == 8
+        @test MendelKinship.identity_state([a, b], [c, d]) == 9
+    end
+end
+
+@testset "delta7_matrix" begin
+    keyword = set_keyword_defaults!(Dict{AbstractString, Any}())
+    keyword["kinship_output_file"] = "Kinship_Output_File.txt"
+    process_keywords!(keyword, "kinship_Control.txt", "")
+    (pedigree, person, nuclear_family, locus, snpdata,
+    locus_frame, phenotype_frame, pedigree_frame, snp_definition_frame) =
+        read_external_data_files(keyword)
+
+    kinship_matrix = MendelKinship.kinship_matrix(pedigree, person, 1, false)
+    delta7_matrix = MendelKinship.delta7_matrix(pedigree, person, kinship_matrix, 1)
+    #
+    #   This seems to be the fraternity coefficient. It is the probability that 
+    #   two individuals share both of their alleles identity by descent.
+    #
+
+
+end
+
+@testset "jacquard_coefficients" begin
+    keyword = set_keyword_defaults!(Dict{AbstractString, Any}())
+    keyword["kinship_output_file"] = "Kinship_Output_File.txt"
+    process_keywords!(keyword, "kinship_Control.txt", "")
+    (pedigree, person, nuclear_family, locus, snpdata,
+    locus_frame, phenotype_frame, pedigree_frame, snp_definition_frame) =
+        read_external_data_files(keyword)
+
+    MendelKinship.jacquard_coefficients(pedigree, person, 1, 1000, false) #1000 repetition = default
+
+
+end
+
+@testset "cotwin_extension!" begin
     
 end
 
