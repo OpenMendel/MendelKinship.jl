1e5dcc75b446f98b9907d0a02784a0c17dde3ab2
Unknown <benchu99@hotmail.com>
2020-08-17 01:12:12 -0700

    fix bug for deploying docs (#5)

--- docs/make.jl
+++ docs/make.jl
@@ -3,23 +3,19 @@ using Documenter, MendelKinship
 ENV["DOCUMENTER_DEBUG"] = "true"
 
 makedocs(
-    format = :html,
+    format = Documenter.HTML(),
     sitename = "MendelKinship",
     modules = [MendelKinship],
     authors = "Jeanette Papp",
     clean = true,
-    debug = true,
     pages = [
         "Home" => "index.md",
-        "API" => "man/api.md"
+        "API" => "man/api.md",
         "Tutorial" => "man/KinshipTutorial.md"
     ]
 )
 
 deploydocs(
     repo   = "github.com/OpenMendel/MendelKinship.jl.git",
-    target = "build",
-    julia  = "1.0",
-    deps   = nothing,
-    make   = nothing
+    target = "build"
 )
